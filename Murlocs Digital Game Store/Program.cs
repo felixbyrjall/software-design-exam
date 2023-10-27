90d4c8f14bf3beccffb9e241299dd3aa321bf9d0
Felix Byrjall <felixbyrjall@gmail.com>
2023-10-27 14:35:50 +0200

    Menu system with Up/Down arrow select

--- Murlocs Digital Game Store/Program.cs
+++ Murlocs Digital Game Store/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace InventoryManager;
+﻿using DigitalGameStore.UI;
+namespace DigitalGameStore;
 
 class Program
 {
 	static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		Start startMenu = new Start();
+		startMenu.StartMenu();
 	}
 }
