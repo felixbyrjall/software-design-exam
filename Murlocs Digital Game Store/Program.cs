a2068e3ab83b86d90e911c8ba6b09c1b8a0e4ca5
Felix Byrjall <felixbyrjall@gmail.com>
2023-10-27 13:00:46 +0200

    Added a UI folder and Menu.cs

--- Murlocs Digital Game Store/Program.cs
+++ Murlocs Digital Game Store/Program.cs
@@ -2,9 +2,8 @@
 
 class Program
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+	}
 }
-
---
+++ Murlocs Digital Game Store/UI/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DigitalGameStore.UI
+{
+	public class Menu
+	{
+		public Menu()
+		{
+		}
+	}
+}
+
