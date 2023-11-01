1f9e83455a3fcb72d6e288c0c842271141e26a57
Jokubas Gedminas <jokas3@gmail.com>
2023-11-01 13:57:06 +0100

    pulled feature-fredrik changes to feature-jokubas

--- Murlocs Digital Game Store/Program.cs
+++ Murlocs Digital Game Store/Program.cs
@@ -1,18 +1,5 @@
 ﻿using DB;
 using DigitalGameStore.UI;
 
-<<<<<<< HEAD
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, Henry!");
-    }
-}
-=======
 Start startMenu = new Start();
 		startMenu.StartMenu();
-		
-		
->>>>>>> feature-fredrik
-
