28422611a0b725f2208d6ad078228498863f5859
NXTDTA <Henry@nextdata.no>
2023-11-04 17:57:46 +0100

    added stuff

---
+++ Murlocs Digital Game Store/DB/Game.cs
@@ -0,0 +1,20 @@
+﻿using DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalGameStore.DB
+{
+    public class Game
+    {
+        public int Game_Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int GenreId { get; set; }
+        public int PublisherId { get; set; }
+        public int Score { get; set; } // Hold the score for interestlist
+
+
+    }
+}
