60ee2d7305dfa8162a677ea5970b4c476941bc4d
NXTDTA <Henry@nextdata.no>
2023-11-08 11:12:30 +0100

    sketched out MVC

--- Murlocs Digital Game Store/DB/Context.cs
+++ Murlocs Digital Game Store/DB/Context.cs
@@ -26,18 +26,6 @@ public partial class Context : DbContext
         modelBuilder.Entity<Genre>(entity => { entity.HasKey(e => e.ID); });
         modelBuilder.Entity<GameGenres>(entity => { entity.HasKey(e => e.ID); });
         modelBuilder.Entity<Interest>(entity => { entity.HasKey(e => e.ID); });
-
-        /* entity.Property(e => e.Name);
-         entity.Property(e => e.Price);
-         entity.Property(e => e.Date);
-         entity.HasOne(d => d.Publiser_ID).WithMany(p =>p.PublisherID)
-             .HasForeignKey(d => d.Publiser_ID);
-     });
-     modelBuilder.Entity<Publisher>(entity =>
-     {
-         entity.HasKey(e => e.PublisherID);
-         entity.Property(e => e.Name);
-     });*/
     }
 
 }
---
+++ Murlocs Digital Game Store/MVC/Views/BrowseView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalGameStore.MVC.Views
+{
+    internal class BrowseView
+    {
+    }
+}
