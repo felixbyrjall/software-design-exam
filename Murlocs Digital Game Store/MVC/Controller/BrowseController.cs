3f7cbd1ebd765164dcd53d1a9c153925abd2a1c2
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-09 17:15:05 +0100

    Removed “magic numbers”

--- Murlocs Digital Game Store/MVC/Controller/BrowseController.cs
+++ Murlocs Digital Game Store/MVC/Controller/BrowseController.cs
@@ -6,10 +6,10 @@ namespace DigitalGameStore.Controller
 {
     public class BrowseController
     {
-        private readonly BrowseView? _browseView; // Kobler seg til View, ergo Middleman
-
 		private readonly GameRepo _gameRepo = new();
 		public static int _currentPage = 10;
+		private int _lastPage = 100;
+		private int _firstPage = 10;
 
 
 		private void AddGames(IEnumerable<Game> games)
@@ -18,22 +18,21 @@ namespace DigitalGameStore.Controller
 			{
 				BrowseView._allGames.Add("ID: " + game.ID + " Name: " + game.Name);
 			}
-		
         }
 
         public void ListGames()
         {
             var games = _gameRepo.GetAllGames((_currentPage - 9), _currentPage); // Henter spill fra GetAllGames metoden inne i GameRepo
-            AddGames(games); // Etter å ha fått spillene, kaller den på metoden inne i view for å display.
+            AddGames(games); //Kaller på metoden AddGames for å legge til spill i _allGames feltet i view.
         }
 
 		public void Check(int i)
 		{
-			if(i == 1 && _currentPage != 100)
+			if(i == 1 && _currentPage != _lastPage)
 			{
 				_currentPage += 10;
 			}
-			else if(i == 2 && _currentPage != 10)
+			else if(i == 2 && _currentPage != _firstPage)
 			{
 				_currentPage -= 10;
 			}
