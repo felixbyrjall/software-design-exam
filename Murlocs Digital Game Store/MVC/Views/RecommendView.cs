04950f41368020065e8b7eec04cc7ffa0b7bb49e
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-24 17:17:46 +0100

    Recommend view displays game info from controller through gamedisplay

--- Murlocs Digital Game Store/MVC/Views/RecommendView.cs
+++ Murlocs Digital Game Store/MVC/Views/RecommendView.cs
@@ -1,12 +1,26 @@
+using DigitalGameStore.Interfaces;
 using DigitalGameStore.Model;
 using DigitalGameStore.Tools;
 
-namespace DigitalGameStore.Views; 
+namespace DigitalGameStore.Views;
 
 public class RecommendView
 {
-    public void ShowGame(GameObject game)
+	private readonly GameDisplay _gameDisplay;
+
+	public RecommendView(GameDisplay gameDisplay)
+	{
+		_gameDisplay = gameDisplay;
+	}
+
+	public void ShowGame(GameObject game)
     {
-        GameDisplay.ShowGameDetails(game);
-    }
+        Console.Clear();
+        Console.WriteLine(_gameDisplay.ShowGameDetails2(game));
+		Func.TextColor("blue");
+		Console.WriteLine("\nPress ENTER to continue");
+		Console.ResetColor();
+		Console.ReadLine();
+
+	}
 }
