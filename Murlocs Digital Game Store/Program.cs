587f1c8eb1bbcdd75a929e8b1596351866f0560b
Felix Byrjall <felixbyrjall@gmail.com>
2023-11-25 15:04:13 +0100

    Restructured the program file, and deleted menu.cs (copy of menuController)

--- Murlocs Digital Game Store/Program.cs
+++ Murlocs Digital Game Store/Program.cs
@@ -11,23 +11,24 @@ public class Program
     public static void Main(string[] args)
     {
         var context = new Context();
-        var gameRepo = new GameRepo(context);
-        var browseView = new BrowseView();
-		var interestRepo = new InterestRepo(context);
 		var menuLogic = new MenuLogic();
-		var interestView = new InterestView();
-        var gameDisplay = new GameDisplay();
 
-		var recommendView = new RecommendView(gameDisplay);
+		var gameRepo = new GameRepo(context);
 		var gameGenresRepo = new GameGenresRepo(context);
-		var browseController = new BrowseController(gameRepo, browseView, interestRepo, menuLogic, gameDisplay);
-		var interestController = new InterestController(interestRepo, interestView, gameRepo, browseController, menuLogic, gameDisplay);
-        var recommendController = new RecommendController(gameGenresRepo, gameRepo, recommendView, interestController, gameDisplay);
+		var interestRepo = new InterestRepo(context);
 
+		var gameInfoView = new GameInfoView();
+		var browseView = new BrowseView();
+		var interestView = new InterestView();
+		var recommendView = new RecommendView(gameInfoView);
+		
+		var browseController = new BrowseController(gameRepo, browseView, interestRepo, menuLogic, gameInfoView);
+		var interestController = new InterestController(interestRepo, interestView, gameRepo, browseController, menuLogic, gameInfoView);
+        var recommendController = new RecommendController(gameGenresRepo, gameRepo, recommendView, interestController, gameInfoView);
         
-        var menu = new Menu(menuLogic, browseController, interestController, recommendController, interestRepo);
+        var menuController = new MenuController(menuLogic, browseController, interestController, recommendController, interestRepo);
 
         // Run the main menu
-        menu.MainMenu();
+        menuController.MainMenu();
     }
 }
