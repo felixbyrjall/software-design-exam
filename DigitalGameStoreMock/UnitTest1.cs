341e7ebbf46cd695dbe2ffe04930960c95148a80
fredrikgaaso <fredrikravndalgaaso@gmail.com>
2023-11-27 20:29:17 +0100

    mock tests done?

--- DigitalGameStoreMock/UnitTest1.cs
+++ DigitalGameStoreMock/UnitTest1.cs
@@ -11,20 +11,20 @@ public class Tests {
     public void Test_GetGameInfoMock()
     {
         // Arrange
-        int gameId = 1;
         var mockGameRepo = new Mock<IGameRepo>();
 
         GameObject expectedGame = new()
         {
+            ID = 1,
             Name = "Counter-Strike 2",
             Publisher = "Valve",
             ReleaseDate = "2012-08-21",
             Genres = new List<string> { "FPS", "Shooter", "Multiplayer", "Competitive", "Action" }
         };
-        mockGameRepo.Setup(r => r.GetGameInfo(gameId)).Returns(expectedGame);
+        mockGameRepo.Setup(r => r.GetGameInfo(expectedGame.ID)).Returns(expectedGame);
         
         // Act
-        GameObject gameInfo = mockGameRepo.Object.GetGameInfo(gameId);
+        GameObject gameInfo = mockGameRepo.Object.GetGameInfo(expectedGame.ID);
 
         // Assert
         Assert.Multiple(() =>
@@ -36,14 +36,18 @@ public class Tests {
         });
     }
     [Test]
-    public void Test_GetNotInterestedGames_AddGameToInterest_Mock()
+    public void Test_GetNotInterestedGames_Mock()
     {
         // Arrange
         var mockInterestRepo = new Mock<IInterestRepo>();
-        var newInterest = new Interest { GameID = 3 };
-
-        mockInterestRepo.Setup(r => r.AddGameToInterest(newInterest.GameID));
-
+        var newInterest = new GameObject
+        {
+            ID = 3,
+            Name = "Dota 2",
+            Publisher = "Valve",
+            ReleaseDate = "2013-07-09",
+            Genres = new List<string>{"MOBA", "Multiplayer", "Strategy", "PVP", "Team-Based"},
+        };
         mockInterestRepo.Setup(r => r.GetNotInterestedGames()).Returns(new List<GameObject>
         {
             new GameObject
@@ -63,19 +67,33 @@ public class Tests {
                 ReleaseDate = "2013-07-09",
                 Genres = new List<string>{"MOBA", "Multiplayer", "Strategy", "PVP", "Team-Based"},
 
-            } 
+            }
         });
 
         // Act
-        mockInterestRepo.Object.AddGameToInterest(newInterest.GameID);
         var notInterestedList = mockInterestRepo.Object.GetNotInterestedGames();
 
         // Assert
         foreach (var item in notInterestedList)
         {
-            Console.WriteLine(item.Name);
-            Assert.That(item.ID, Is.Not.EqualTo(newInterest.GameID));
+            Assert.That(item.ID, Is.Not.EqualTo(newInterest.ID));
         }
     }
 
+    [Test]
+    public void Test_GetIntGenres_Mock() {
+        //Arrange
+        var mockGetIntGenres = new Mock<IGameGenreRepo>();
+        var expectedGenres = new List<int>
+        {
+            1,2,3,4,5
+        };
+        mockGetIntGenres.Setup(g => g.GetIntGenres()).Returns(expectedGenres);
+      //Act
+       var genres = mockGetIntGenres.Object.GetIntGenres();
+       foreach (var item in genres) {
+           //Assert
+           Assert.That(genres, Is.EqualTo(expectedGenres));
+       }
+    }
 }
